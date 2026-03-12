7c35399754a0401b3ea71edbf992d91214effa6a
NajlaHanifahMuhtadi <najlahanifahm@gmail.com>
2026-03-12 11:03:15 +0700

    prosedur insertion sort

--- file.cpp
+++ file.cpp
@@ -26,3 +26,22 @@ void input (){      //Membuat fungsi bernama input. void berarti fungsi tidak me
         cin >> arr[i];      //Nilai yang dimasukkan disimpan ke array pada indeks ke-i.
     }
 }
+
+void insertionsort(){       //prosedur insertion sort
+    int temp;
+    int j;
+
+    for (int i = 0; i < n; i++){
+        temp = arr[i];
+        j = i-1;
+        while (j >= 0 && arr[j] > temp){
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i << ": ";
+        for (int k = 0; k < n; k++){
+            cout << arr[k] << " ";
+        }
+    }
+}
